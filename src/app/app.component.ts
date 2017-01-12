ce5de15c8e2fcf54064c3a323bf8c9cee1ca4d4b
shogunfighter <rene.cabrera.jr@gmail.com>
2017-01-12 18:50:11 +0800

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //moduleId: module.id,
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <nav>
+      <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+      <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
+  styles: [`
+    h1 {
+      font-size: 1.2em;
+      color: #999;
+      margin-bottom: 0;
+    }
+    h2 {
+      font-size: 2em;
+      margin-top: 0;
+      padding-top: 0;
+    }
+    nav a {
+      padding: 5px 10px;
+      text-decoration: none;
+      margin-top: 10px;
+      display: inline-block;
+      background-color: #eee;
+      border-radius: 4px;
+    }
+    nav a:visited, a:link {
+      color: #607D8B;
+    }
+    nav a:hover {
+      color: #039be5;
+      background-color: #CFD8DC;
+    }
+    nav a.active {
+      color: #039be5;
+    }
+  `],
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Tour of Heroes';
 }
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
